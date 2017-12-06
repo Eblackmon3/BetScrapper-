8560a3af83dd2f8a4698338cb8ceb0bc11500592
Eric Blackmon <ericblackmon38@gmail.com>
2017-12-05 19:20:21 -0600

    first commit

---
+++ src/main/java/Application/ScrapperController.java
@@ -0,0 +1,14 @@
+package Application;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class ScrapperController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
