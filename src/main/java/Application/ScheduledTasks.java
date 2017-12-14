8dece8a52e2127b958d8612345036fb5e4868804
Eric Blackmon <ericblackmon38@gmail.com>
2017-12-14 10:51:11 -0600

    first commit

--- src/main/java/Application/ScheduledTasks.java
+++ src/main/java/Application/ScheduledTasks.java
@@ -86,6 +86,7 @@ public class ScheduledTasks {
             URL url = new URL("http://example.com");
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
+            con.disconnect();
         }catch(Exception e){
             e.printStackTrace();
         }
